7c697b7fe55dc7a853c5aeedccb3ef42b3f6eeff
bea94k <beatakusnierz94@gmail.com>
2020-08-12 21:40:38 +0300

    Changed LogIn, Register and NewPost into functional components (after merging from class-components branch)

--- social-network-app/src/Components/Auth/Register.js
+++ social-network-app/src/Components/Auth/Register.js
@@ -1,94 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-class Register extends React.Component {
-  constructor(props) {
-    super(props);
+const Register = () => {
+  const [registerUser, setRegisterUser] = useState({
+    firstname: null,
+    lastname: null,
+    email: null,
+    password: null,
+  });
 
-    this.state = {
-      firstname: null,
-      lastname: null,
-      email: null,
-      password: null,
-    };
-
-    this.handleSubmission = this.handleSubmission.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange = (e) => {
-    this.setState({
+  const handleChange = (e) => {
+    setRegisterUser({
+      ...registerUser,
       [e.target.id]: e.target.value,
     });
   };
 
-  handleSubmission = (e) => {
+  const handleSubmission = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    console.log(registerUser);
   };
 
-  render() {
-    return (
-      <div>
-        <div className="center">
-          <h5>Register</h5>
-          <p>
-            If you already have an account, <Link to="/login">log in here</Link>
-            .
-          </p>
-        </div>
-        <div className="container row">
-          <div className="col s12 m8 offset-m2 l6 offset-l3">
-            <form onSubmit={this.handleSubmission}>
-              <div className="input-field">
-                <input
-                  id="firstname"
-                  type="text"
-                  className="validate"
-                  onChange={this.handleChange}
-                />
-                <label htmlFor="firstname">First Name</label>
-              </div>
-              <div className="input-field">
-                <input
-                  id="lastname"
-                  type="text"
-                  className="validate"
-                  onChange={this.handleChange}
-                />
-                <label htmlFor="lastname">Last Name</label>
-              </div>
-              <div className="input-field">
-                <input
-                  id="email"
-                  type="email"
-                  className="validate"
-                  onChange={this.handleChange}
-                />
-                <label htmlFor="username">Email</label>
-              </div>
-              <div className="input-field">
-                <input
-                  id="password"
-                  type="password"
-                  className="validate"
-                  onChange={this.handleChange}
-                />
-                <label htmlFor="password">Password</label>
-              </div>
-              <button
-                className="btn waves-effect waves-purple btn-large deep-purple"
-                type="submit"
-                name="action"
-              >
-                Register
-              </button>
-            </form>
-          </div>
+  return (
+    <div>
+      <div className="center">
+        <h5>Register</h5>
+        <p>
+          If you already have an account, <Link to="/login">log in here</Link>.
+        </p>
+      </div>
+      <div className="container row">
+        <div className="col s12 m8 offset-m2 l6 offset-l3">
+          <form onSubmit={handleSubmission}>
+            <div className="input-field">
+              <input
+                id="firstname"
+                type="text"
+                className="validate"
+                onChange={handleChange}
+              />
+              <label htmlFor="firstname">First Name</label>
+            </div>
+            <div className="input-field">
+              <input
+                id="lastname"
+                type="text"
+                className="validate"
+                onChange={handleChange}
+              />
+              <label htmlFor="lastname">Last Name</label>
+            </div>
+            <div className="input-field">
+              <input
+                id="email"
+                type="email"
+                className="validate"
+                onChange={handleChange}
+              />
+              <label htmlFor="username">Email</label>
+            </div>
+            <div className="input-field">
+              <input
+                id="password"
+                type="password"
+                className="validate"
+                onChange={handleChange}
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+            <button
+              className="btn waves-effect waves-purple btn-large deep-purple"
+              type="submit"
+              name="action"
+            >
+              Register
+            </button>
+          </form>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Register;
--- social-network-app/src/Components/Posts/NewPost.js
+++ social-network-app/src/Components/Posts/NewPost.js
@@ -1,58 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-class NewPost extends React.Component {
-  constructor(props) {
-    super(props);
+const NewPost = () => {
+  const [newPost, setNewPost] = useState({
+    postTitle: null,
+    postContent: null,
+  });
 
-    this.state = {
-      postTitle: null,
-      postContent: null,
-    };
-
-    this.handleSubmission = this.handleSubmission.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange = (e) => {
-    this.setState({
+  const handleChange = (e) => {
+    setNewPost({
+      ...newPost,
       [e.target.id]: e.target.value,
     });
   };
 
-  handleSubmission = (e) => {
+  const handleSubmission = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    console.log(newPost);
   };
 
-  render() {
-    return (
-      <div className="container row">
-        <div className="col s12 m8 offset-m2 l6 offset-l3">
-          <form onSubmit={this.handleSubmission}>
-            <div className="input-field">
-              <input id="postTitle" type="text" onChange={this.handleChange} />
-              <label htmlFor="postTitle">Post Title:</label>
-            </div>
-            <div className="input-field">
-              <textarea
-                id="postContent"
-                className="materialize-textarea"
-                onChange={this.handleChange}
-              ></textarea>
-              <label htmlFor="postContent">Post:</label>
-            </div>
-            <button
-              className="btn waves-effect waves-purple btn-large deep-purple"
-              type="submit"
-              name="action"
-            >
-              Submit
-            </button>
-          </form>
-        </div>
+  return (
+    <div className="container row">
+      <div className="col s12 m8 offset-m2 l6 offset-l3">
+        <form onSubmit={handleSubmission}>
+          <div className="input-field">
+            <input id="postTitle" type="text" onChange={handleChange} />
+            <label htmlFor="postTitle">Post Title:</label>
+          </div>
+          <div className="input-field">
+            <textarea
+              id="postContent"
+              className="materialize-textarea"
+              onChange={handleChange}
+            ></textarea>
+            <label htmlFor="postContent">Post:</label>
+          </div>
+          <button
+            className="btn waves-effect waves-purple btn-large deep-purple"
+            type="submit"
+            name="action"
+          >
+            Submit
+          </button>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewPost;
